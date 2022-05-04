47cd86ac12ef74ece5f66d1b5503f09bd4c41ac8
Zouhair <ettarak.zouhair@gmail.com>
2022-05-04 19:02:23 +0000

    add Repository rest controller and methode search by Type

--- src/main/java/com/ettarak/compte/service/Repositories/CompteRepository.java
+++ src/main/java/com/ettarak/compte/service/Repositories/CompteRepository.java
@@ -1,7 +1,16 @@
 package com.ettarak.compte.service.Repositories;
 
 import com.ettarak.compte.service.Entities.Compte;
+import com.ettarak.compte.service.Enums.TypeCompte;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
 
+import java.util.List;
+
+@RepositoryRestController
 public interface CompteRepository extends JpaRepository<Compte, Long> {
+    @RestResource(path = "/ByType")
+    List<Compte> findByType(@Param(value="type")TypeCompte typeCompte);
 }
